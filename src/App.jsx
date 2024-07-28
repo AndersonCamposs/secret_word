ace0d028d29fa5890af9405a7ede9d13feb8f173
AndersonCamposs <anderson.camposs87@outlook.com>
2024-07-28 14:42:18 -0300

    Criando os demais componentes da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 //Component imports
 import StartScreen from './components/StartScreen'
+import Game from './components/Game'
+import GameOver from './components/GameOver'
+//Hooks imports 
+import {useCallback, useEffect, useState} from 'react';
+//Data imports
+import { wordsList } from './data/words';
 //Style imports
 import './App.css'
 
+const stages = [
+  {id: 1, name: 'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'}
+]
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+  console.log(words)
   return (
     <div className='App'>
-      <StartScreen/>
+    {gameStage === 'start' && <StartScreen/>}
+    {gameStage === 'game' && <Game/>}
+    {gameStage === 'end' && <GameOver/>}
     </div>
   )
 }
